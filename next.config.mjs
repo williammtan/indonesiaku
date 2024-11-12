b5c1d8169bfdfd9373b467b5134cf10335d267c8
williammtan <william@tan.id>
2024-11-12 19:15:31 +0700

    fix next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,6 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
-
-module.exports = {
-	output: "standalone",
+const nextConfig = {
+    output: 'standalone'
 };
 
 export default nextConfig;
