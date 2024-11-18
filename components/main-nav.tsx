9245844189b2f6874ccb1bd04b418f2ec2bdd6ae
williammtan <william@tan.id>
2024-11-18 21:19:37 +0700

    change Ku to ku

--- components/main-nav.tsx
+++ components/main-nav.tsx
@@ -17,7 +17,7 @@ export function MainNav() {
       <Link href="/" className="mr-4 flex items-center space-x-2 lg:mr-6">
         <Icons.logo className="h-6 w-6" />
         <span className="hidden font-bold lg:inline-block">
-          IndonesiaKu
+          Indonesiaku
         </span>
       </Link>
       <nav className="flex items-center gap-4 text-sm lg:gap-6">
